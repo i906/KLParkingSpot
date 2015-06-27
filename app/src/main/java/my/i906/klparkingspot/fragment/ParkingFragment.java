385de3b3eb62e303fc70a2e50aef34dfec32a8a8
Noorzaini Ilhami <ilhami@terato.com>
2015-06-27 12:39:21 +0800

    Added error view, closes issue #2

--- app/src/main/java/my/i906/klparkingspot/fragment/ParkingFragment.java
+++ app/src/main/java/my/i906/klparkingspot/fragment/ParkingFragment.java
@@ -2,15 +2,22 @@ package my.i906.klparkingspot.fragment;
 
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import butterknife.InjectView;
+import butterknife.OnClick;
+import my.i906.klparkingspot.R;
 import my.i906.klparkingspot.adapter.MallAdapter;
 import my.i906.klparkingspot.model.Mall;
 import my.i906.klparkingspot.model.Pgis;
 import my.i906.klparkingspot.view.DividerItemDecoration;
+import retrofit.RetrofitError;
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
@@ -24,10 +31,21 @@ public class ParkingFragment extends BaseRecyclerFragment {
     private LinearLayoutManager mLayoutManager;
     private MallAdapter mAdapter;
 
+    @InjectView(R.id.error_container)
+    protected View mErrorContainer;
+
+    @InjectView(R.id.tv_error)
+    protected TextView mErrorMessageView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAdapter = new MallAdapter();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_parking, container, false);
     }
 
     @Override
@@ -59,17 +77,62 @@ public class ParkingFragment extends BaseRecyclerFragment {
                     @Override
                     public void onError(Throwable e) {
                         e.printStackTrace();
+
+                        if (e instanceof RetrofitError) {
+                            RetrofitError error = (RetrofitError) e;
+                            switch (error.getKind()) {
+                                case NETWORK:
+                                    mErrorMessageView.setText(R.string.error_no_network);
+                                    break;
+                                case CONVERSION:
+                                    mErrorMessageView.setText(R.string.error_conversion);
+                                    break;
+                                case HTTP:
+                                    mErrorMessageView.setText(R.string.error_http);
+                                    break;
+                                default:
+                                    mErrorMessageView.setText(R.string.error_unexpected);
+                            }
+                        } else {
+                            mErrorMessageView.setText(R.string.error_unexpected);
+                        }
+
+                        setContentVisibility(false, true);
+                        setProgressVisibility(false, true);
+                        setErrorVisibility(true, true);
                     }
 
                     @Override
                     public void onNext(List<Mall> malls) {
                         mAdapter.setMallList(malls);
                         mListContainer.setRefreshing(false);
-                        setListShown(true, true);
+                        setContentVisibility(true, true);
+                        setProgressVisibility(false, true);
+                        setErrorVisibility(false, true);
                     }
                 });
 
         mSubscription.add(s);
+    }
+
+    @OnClick(R.id.btn_retry)
+    protected void onRetryButtonClicked() {
+        onRefresh();
+        setContentVisibility(false, true);
+        setProgressVisibility(true, true);
+        setErrorVisibility(false, true);
+    }
+
+    private void setContentVisibility(boolean visible, boolean animate) {
+        setViewVisibility(mListContainer, visible, animate);
+    }
+
+    private void setProgressVisibility(boolean visible, boolean animate) {
+        setViewVisibility(mProgressContainer, visible, animate);
+    }
+
+    private void setErrorVisibility(boolean visible, boolean animate) {
+        setViewVisibility(mErrorContainer, visible, animate);
     }
 
     @Override
